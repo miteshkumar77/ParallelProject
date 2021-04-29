0f2ed4750a7aed6e2b238729dc7c371a72c646a3
Mitesh Kumar <miteshkumarca@gmail.com>
2021-04-29 14:36:28 -0700

    Add support for normal and exponential distributions in data_gen.c

--- data_gen.c
+++ data_gen.c
@@ -4,26 +4,35 @@
  * want to parallelize this later for larger
  * data sets
  */
+
+#define _XOPEN_SOURCE
+#include <math.h>
+#include <time.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <limits.h>
 #include "./serial_sort.h"
 
 #define BUFF_SZ 1024
 
 /* lower bound */
-int lb;
+elem lb;
 
 /* upper bound */
-int ub;
+elem ub;
 
 /* number of numbers */
 ssize_t n;
 
 /* file path */
 char* fpath;
+
+/* distribution shape */
+char* distribution;
 
 /* file descriptor */
 int fd;
@@ -34,22 +43,83 @@ int rc;
 /* write buffer */
 elem* wbuff;
 
+/* normal constants */
+elem norm2;
+int done = 0;
+
+/* exponential constants */
+const double lambda = 0.01;
 
 ssize_t min(ssize_t a, ssize_t b) { return a < b ? a : b; }
 
+elem (*genPtr)();
 
-/* usage [ executable ] [ lowest number ] [ highest number ] [ number of points ] [ fpath ] */
+elem get_uniform() {
+	return ((elem)rand()) % (ub - lb + 1) + lb;
+}	
+
+elem get_normal() {
+#if 0
+	if (done) {
+		done = 0;
+		return norm2;
+	}
+
+	double x,y,rsq,f;
+	do {
+		x = 2.0 * rand() / (double)RAND_MAX - 1.0;
+    y = 2.0 * rand() / (double)RAND_MAX - 1.0;
+		rsq = x * x + y * y;
+	} while (rsq >= 1. || rsq == 0.);
+	f = sqrt(-2.0 * log(rsq)/rsq);
+	norm2 = (elem)((y * f * (double)(ub - lb)) + ((double)(lb) + ((double)(ub - lb))/2));
+	done = 1;
+	return (elem)((x * f * (double)(ub - lb)) + ((double)(lb) + ((double)(ub - lb))/2));
+#endif
+
+	double x = (double)rand() / RAND_MAX;
+	double y = (double)rand() / RAND_MAX;
+	double z = sqrt(-2 * log(x)) * cos(2 * M_PI * y);
+	double lb_dbl = lb;
+	double ub_dbl = ub;
+	return (elem)(z * (ub_dbl - lb_dbl) + (lb_dbl + (ub_dbl - lb_dbl)/2));
+}
+
+elem get_exp() {
+	double res = INT_MAX;
+	while (res > (double)ub) {
+		res = lb + floor(-log(drand48()) / lambda); 
+	}
+	return (elem)res;
+}
+
+/* usage [ executable ] [ lowest number ] [ highest number ] [ number of points ] [ distribution ] [ fpath ] */
 int main(int argc, char** argv) {
-	if (argc != 5) {
+	if (argc != 6) {
 		fprintf(stderr, "ERROR: invalid argument(s)\n");
-		fprintf(stderr, "USAGE: [ executable ] [ lowest number ] [ highest number ] [ number of points ] [ fpath ]\n");
+		fprintf(stderr, "USAGE: [ executable ] [ lowest number ] [ highest number ] [ number of points ] [ distribution ] [ fpath ]\n");
 		return EXIT_FAILURE;
 	}
 	
+	srand48(time(0));
 	lb    = atoi(argv[1]);
 	ub    = atoi(argv[2]);
 	n     = atoi(argv[3]);
-	fpath = argv[4];
+	distribution = argv[4];
+	fpath = argv[5];
+	
+	if (0 == strcmp(distribution, "uniform")) {
+		genPtr = &get_uniform;
+	} else if (0 == strcmp(distribution, "normal")) {
+		genPtr = &get_normal;
+	} else if (0 == strcmp(distribution, "exponential")) {
+		genPtr = &get_exp;
+	} else {
+		fprintf(stderr, "ERROR: unknown distribution\n");
+		return EXIT_FAILURE;
+	}
+
+
 	
 	fd = open(fpath, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1) {
@@ -69,10 +139,10 @@ int main(int argc, char** argv) {
 	for (int i = 0; i < n; i += BUFF_SZ) {
 		num_elems = min(BUFF_SZ, n - i);	
 		for (int j = 0; j < num_elems; ++j) {
-			wbuff[j] = rand() % (ub - lb + 1) + lb;
+			wbuff[j] = (*genPtr)();
 			
 #ifdef DEBUG_MODE	
-			printf(" %d", wbuff[j]);
+			printf(" %d,", wbuff[j]);
 #endif
 		}
 		rc = write(fd, wbuff, num_elems * sizeof(elem));
